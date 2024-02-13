4855b7b4183bd82ab4e2c36b8cbd658ce94cfcdb
username <wecp-dummy@gmail.com>
2024-02-13 05:10:01 +0000

    Day20 Commit

--- frontend/src/app/bank/components/account/account.component.ts
+++ frontend/src/app/bank/components/account/account.component.ts
@@ -1,5 +1,35 @@
+// src/app/accounts/accounts.component.ts
+import { Component, OnInit } from '@angular/core';
+import { AccountTS } from '../../types/tstypes/Accountts';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
-export class AccountComponent {
- 
+@Component({
+  selector: 'app-accounts',
+  templateUrl: './account.component.html',
+  styleUrls: ['./account.component.scss']
+})
+export class AccountComponent implements OnInit {
+onSubmit() {
+throw new Error('Method not implemented.');
+}
+  accountForm!: FormGroup;
+  account: AccountTS | undefined;
+  // accountForm: any;
+  formBuilder!: FormBuilder;
+  validateAccountId: any;
+  validateNonNegativeAmount: any;
+  // account: Account;
 
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit(): void {
+    // No need to fetch data from a service since we are using hardcoded data
+    this.accountForm = this.fb.group({
+      account_id: ["", [Validators.required]],
+      customer_id: ["", [Validators.required]],
+      balance: ["", [Validators.required]],
+    });
+    this.account = new AccountTS("1" ,1000.00,"1");
+
+  }
 }
--- frontend/src/app/bank/components/customerarray/customerarray.component.ts
+++ frontend/src/app/bank/components/customerarray/customerarray.component.ts
@@ -1,15 +1,16 @@
 import { Component } from '@angular/core';
-import { FormGroup, FormBuilder, Validators } from '@angular/forms';
-import { of } from 'rxjs';
-import { Customer } from '../../types/Customer';
+// import { Customer } from '../bank/types/Customer';
+import { CustomerTS } from '../../types/tstypes/Customerts';
 
 @Component({
   selector: 'app-customerarray',
-  standalone: true,
-  imports: [],
+
   templateUrl: './customerarray.component.html',
-  styleUrl: './customerarray.component.css'
+  styleUrls: ['./customerarray.component.css']
 })
 export class CustomerarrayComponent {
-  
+  customers: CustomerTS[] = [
+    new CustomerTS("John Doe", "john@example.com", "john_doe", "password123", "User", "1"),
+    new CustomerTS("John Doe1", "john1@example.com", "john_doe", "password123", "Admin", "2")
+   ]
 }
